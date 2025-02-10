c80511421405a09362618861c0be8aeee90524f9
CCEPCAHER <franklin_carranza@hotmail.com>
2025-02-10 13:30:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,10 @@
 import express from "express"
 import cors from "cors"
 const app  = express()
-const port = 3000
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
+});
 app.use(cors())
 app.use(express.json())
 
